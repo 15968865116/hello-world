4dfa76ac98ebd5a3c2659669e66d6f8f25df4e07
15968865116 <32892505+15968865116@users.noreply.github.com>
2017-10-19 15:00:19 +0800

    1

---
+++ helloController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace xiangmu1.Controllers
+{
+    public class helloController : Controller
+    {
+        // GET: hello
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public ActionResult hello()
+        {
+            ViewBag.message = "hello world";
+            return View();
+        }
+        public ActionResult hello2()
+        {
+            ViewData["MSG"] = "hello world";
+            return View();
+        }
+    }
+}
